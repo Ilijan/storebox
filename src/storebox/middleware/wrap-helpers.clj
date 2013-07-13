f26d80303ea0be1bd286da2121c9bf26afab43c0
Ilijan <ilianvel@abv.bg>
2013-07-13 16:02:12 +0300

    Rename helpers.clj to wrap-helpers.clj

---
+++ src/storebox/middleware/wrap-helpers.clj
@@ -0,0 +1,35 @@
+(ns storebox.middleware.wrap-helpers)
+
+(defn wrap-if [handler pred wrapper & args]
+  (if pred
+    (apply wrapper handler args)
+    handler))
+
+(defn wrap-failsafe [handler]
+  (fn [req]
+    (try
+      (handler req)
+      (catch Exception e
+        {:status 500
+         :headers {"Content-Type" "text/plain"}
+         :body "Something went wrong o_0\nTeam of highly trained monkeys... NO!"}))))
+
+;; TODO: wrap-... methods for handling different errors with more concrete msgs and change resposne to some error status
+
+(defn wrap-body-copy [handler]
+  (fn [{body :body :as request}]
+    (let [byte-output (java.io.ByteArrayOutputStream.)]
+      (clojure.java.io/copy body byte-output)
+      (let [body-copy (java.io.ByteArrayInputStream. (.toByteArray byte-output)) ;; REVIEW: threading macro
+            body-byte-array (.toByteArray byte-output)
+            request-with-new-body (assoc request :body body-copy)
+            new-request (assoc request-with-new-body :body-bytes body-byte-array)]
+        (handler new-request)))))
+
+(defn wrap-body-str [handler]
+  (fn [{bytes :body-bytes :as request}]
+    (if bytes
+      (let [body-str (apply str (seq (map char bytes)))       ;; REVIEW: threading macro
+            new-request (assoc request :body-str body-str)]
+        (handler new-request))
+      (handler request))))
