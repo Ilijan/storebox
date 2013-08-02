72af39f92aa22fc435172bae14176b677519083c
Ilijan <ilianvel@abv.bg>
2013-08-02 22:24:59 +0300

    Fix comment

--- src/storebox/middleware/makedir.clj
+++ src/storebox/middleware/makedir.clj
@@ -4,5 +4,5 @@
 (defn makedir-handler [root-dir path {create-dirs "create_dirs"}]
   (let [dir-path (-> (concatenate-paths root-dir path)
                      (as-file))]
-    (when-not (if create-dirs (.mkdirs dir-path) (.mkdir dir-path)) ;; REVIEW: if there are subsequent not existient dirs
+    (when-not (if create-dirs (.mkdirs dir-path) (.mkdir dir-path)) ;; REVIEW: if there are subsequent non existent dirs
         (throw (Exception. "directory not created")))))
